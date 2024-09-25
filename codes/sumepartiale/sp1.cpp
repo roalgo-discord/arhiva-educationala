16d4123a0811f9d01aaa09ba8fca2dfd615870e8
Susan <susan@susan.lol>
2024-09-25 13:17:25 +0300

    empty gh-pages

--- codes/sumepartiale/sp1.cpp
+++
@@ -1,2 +0,0 @@
-for (int i = 1; i <= n; i++)
-    sp[i] = sp[i - 1] + v[i];
--- codes/sumepartiale/sp5.cpp
+++
@@ -1,8 +0,0 @@
-mars[x] += z;
-mars[y + 1] -= z;
-/// cod din program
-for (int i = 1; i <= n; i++)
-{
-    mars[i] += mars[i - 1];
-    v[i] += mars[i];
-}
